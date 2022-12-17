a057514bcbaec6f7f4648e56794427a04e0d25cb
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2022-12-17 13:21:10 -0500

    switched to simplex noise

--- src/main_game.py
+++ src/main_game.py
@@ -3,8 +3,8 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from manager import GameManager
 
+import opensimplex as noise
 from random import randint, choice
-from vnoise import Noise
 import time
 
 from .constants import TILE_SIZE, WIDTH, VEC, HEIGHT, FONT
@@ -14,18 +14,17 @@ from .ground import Ground
 from .player import Player
 from .scene import Scene
 
-
 class MainGame(Scene):
     def __init__(self, manager: GameManager, previous_scene: Scene) -> None:
         super().__init__(manager, previous_scene)
         while "seed" not in self.client.thread_data:
             time.sleep(0.01)
-        self.noise = Noise(self.client.thread_data["seed"])
+        noise.seed(self.client.thread_data["seed"])
         self.player = Player(self)
 
         for x in range(-100, 101):
             # Horizontal stretch and vertical stretch (essentially)
-            y = self.noise.noise1(x * 0.05) * 400
+            y = noise.noise2(x * 0.1, 0) * 200
             Ground(self, (x * TILE_SIZE, y), (TILE_SIZE, 500))
         for ground in Ground.instances:
             ground.generate_image() # Create a images only after all tiles have been created
