4ee42e426266b0c3a079b0dafd743564b78ee5b6
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2022-12-20 14:49:52 -0500

    snowballs no longer get points during wait

--- src/snowball.py
+++ src/snowball.py
@@ -70,7 +70,8 @@ class Snowball(VisibleSprite):
         for player in self.manager.other_players.values():
             if player.real_rect.colliderect(self.real_rect):
                 self.kill()
-                self.scene.score += self.score
+                if "ready" in self.manager.client.thread_data:
+                    self.scene.score += self.score
                 return
 
         if self.pos.y > 1000:
