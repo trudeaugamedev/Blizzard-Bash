ef708e7346e6f5c8c6b3b7aeb3b71fa0daa5eb2d
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2022-12-21 10:25:35 -0500

    fixed other player text shadow being bigger

--- src/others.py
+++ src/others.py
@@ -40,7 +40,7 @@ class OtherPlayer(VisibleSprite):
         self.manager.screen.blit(self.image, (*(VEC(self.rect.topleft) - self.scene.player.camera.offset), *self.size))
         text = FONT[28].render(f"{self.score}", True, (0, 0, 0))
         pos = VEC(self.rect.midtop) - (text.get_width() // 2, text.get_height() + 5)
-        text_shadow = FONT[30].render(f"{self.score}", True, (0, 0, 0))
+        text_shadow = FONT[28].render(f"{self.score}", True, (0, 0, 0))
         text_shadow.set_alpha(70)
         self.manager.screen.blit(text_shadow, pos + (3, 3) - self.scene.player.camera.offset)
         self.manager.screen.blit(text, pos - self.scene.player.camera.offset)
