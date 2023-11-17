e464d7b9577fc97aae90cd6aa57762687040a374
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2023-11-17 17:11:31 -0500

    change to heroku server

--- src/client.py
+++ src/client.py
@@ -83,8 +83,8 @@ class Client:
 
     async def connect(self) -> None:
         print("Coroutine 'connect' started")
-        self.socket = await ws_client.connect("ws://localhost:3000")
-        # self.socket = await ws_client.connect("wss://trudeaugamedev-winter.herokuapp.com")
+        # self.socket = await ws_client.connect("ws://localhost:3000")
+        self.socket = await ws_client.connect("wss://trudeaugamedev-winter.herokuapp.com")
         print("Coroutine 'connect' completed")
 
     async def main(self) -> None:
