9a7ef91da7dd30674a42570db3479124df02c287
DaNub <75505891+DaNubCoding@users.noreply.github.com>
2023-11-19 21:56:18 -0500

    better leaderboard display

--- src/end_menu_score.py
+++ src/end_menu_score.py
@@ -11,15 +11,28 @@ import pytweening as tween
 import pygame
 import time
 
+def get_superscript(n: int) -> str:
+    if n in {11, 12, 13}: return "th"
+    str_n = str(n)
+    match str_n[-1]:
+        case "1": return "st"
+        case "2": return "nd"
+        case "3": return "rd"
+        case _: return "th"
+
 class EndMenuScore(VisibleSprite):
     def __init__(self, scene: Scene, index: int, name: str, score: int, mine: bool) -> None:
         super().__init__(scene, Layers.GUI)
         self.pos = VEC(30, 30 + index * 50)
+        self.offset2 = 60
+        self.offset3 = 410
         self.index = index
         self.mine = mine
-        self.text = f"{name}: {score}"
-        self.text_surf = FONT[36].render(self.text, False, (0, 0, 0))
-        self.size = VEC(500, self.text_surf.get_height())
+        self.rank_surf = FONT[36].render(str(index + 1), False, (0, 0, 0))
+        self.rank_ss_surf = FONT[14].render(get_superscript(index + 1), False, (0, 0, 0)) # Superscript
+        self.name_surf = FONT[36].render(str(name), False, (0, 0, 0))
+        self.score_surf = FONT[36].render(str(score), False, (0, 0, 0))
+        self.size = VEC(500, self.rank_surf.get_height())
         self.progress = 0
         self.linear_progress = 0
         self.delay_time = time.time()
@@ -27,7 +40,10 @@ class EndMenuScore(VisibleSprite):
         if self.mine:
             self.white = False
             self.flash_timer = time.time()
-            self.other_text_surf = FONT[36].render(self.text, False, (255, 255, 255))
+            self.white_rank_surf = FONT[36].render(str(index + 1), False, (255, 255, 255))
+            self.white_rank_ss_surf = FONT[14].render(get_superscript(index + 1), False, (255, 255, 255))
+            self.white_name_surf = FONT[36].render(str(name), False, (255, 255, 255))
+            self.white_score_surf = FONT[36].render(str(score), False, (255, 255, 255))
 
     def update(self) -> None:
         if time.time() - self.delay_time < 0.4 + self.index * 0.2: return
@@ -42,11 +58,19 @@ class EndMenuScore(VisibleSprite):
         (surf := pygame.Surface(VEC(self.size.x * self.progress, self.size.y), SRCALPHA)).fill((255, 255, 255, 40))
         self.manager.screen.blit(surf, self.pos)
         pygame.draw.rect(self.manager.screen, (0, 0, 0), (self.pos, (self.size.x * self.progress, self.size.y)), 3)
+        pygame.draw.line(self.manager.screen, (0, 0, 0), self.pos + (self.offset2, 0), self.pos + (self.offset2, self.size.y - 1), 3)
+        pygame.draw.line(self.manager.screen, (0, 0, 0), self.pos + (self.offset3, 0), self.pos + (self.offset3, self.size.y - 1), 3)
         if self.progress > 0.75:
-            self.manager.screen.blit(self.text_surf, self.pos + (12, -5))
+            self.manager.screen.blit(self.rank_surf, self.pos + (12, -5))
+            self.manager.screen.blit(self.name_surf, self.pos + (self.offset2 + 12, -5))
+            self.manager.screen.blit(self.score_surf, self.pos + (self.offset3 + 12, -5))
+            self.manager.screen.blit(self.rank_ss_surf, self.pos + (12, -5) + (self.rank_surf.get_width(), 8))
             if self.mine:
                 if time.time() - self.flash_timer > 0.5:
                     self.flash_timer = time.time()
                     self.white = not self.white
                 if self.white:
-                    self.manager.screen.blit(self.other_text_surf, self.pos + (12, -5))
+                    self.manager.screen.blit(self.white_rank_surf, self.pos + (12, -5))
+                    self.manager.screen.blit(self.white_name_surf, self.pos + (self.offset2 + 12, -5))
+                    self.manager.screen.blit(self.white_score_surf, self.pos + (self.offset3 + 12, -5))
+                    self.manager.screen.blit(self.white_rank_ss_surf, self.pos + (12, -5) + (self.white_rank_surf.get_width(), 8))
