c6758a71c431f4365ac5ad325fa044325e6f690e
ricardolee9 <150194135+ricardolee9@users.noreply.github.com>
2023-12-10 14:57:16 -0500

    Update index.js

---
+++ leaderboard/index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+function generateLeaderboardHTML() {
+    const json_data = JSON.parse(fs.readFileSync('./leaderboard.json'));
+    const playersWithScores = json_data.players.map(player => ({
+        name: player,
+        score: json_data.playersData[player].score
+    }));
+
+    playersWithScores.sort((a, b) => b.score - a.score);
+
+    const htmlContent = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blizzard Bash Leaderboard</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <h1>Blizzard Bash Game Leaderboard</h1>
+    <table>
+        <tr>
+            <th>Username</th>
+            <th>Score</th>
+        </tr>
+        ${playersWithScores.map(player => `
+        <tr>
+            <td>${player.name}</td>
+            <td>${player.score}</td>
+        </tr>`).join('')}
+    </table>
+</body>
+
+</html>`;
+
+    fs.writeFileSync('./index.html', htmlContent);
+}
+
+// Call the function to generate the leaderboard HTML file
+generateLeaderboardHTML();
